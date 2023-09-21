aa2bb219be6cf622d70159bf45c51c265a100b14
Kris Krasnosky <kkrasnosky@gmail.com>
2023-09-21 13:18:59 -0400

    updated startup stuff

--- util/deploy.py
+++ util/deploy.py
@@ -18,6 +18,8 @@ class Deployer():
                             capture_output=True,
                             text=True)
 
+        print(result.stdout)
+
         return result.stdout
 
     @staticmethod
@@ -26,10 +28,22 @@ class Deployer():
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(host, username=username, password="bathyopossum")
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("source /opt/ros/foxy/setup.bash; cd trevor_ws; colcon build")
-        cmd_output = ssh_stdout.read()
-        print('log printing: ', cmd_output)
-        return cmd_output.decode("utf-8")
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("source /opt/ros/foxy/setup.bash; cd trevor_ws; colcon build --symlink-install")
+
+        out = ""
+        def line_buffered(f):
+            line_buf = ""
+            while not f.channel.exit_status_ready():
+                line_buf += f.read(1).decode("utf-8")
+                if line_buf.endswith('\n'):
+                    yield line_buf
+                    line_buf = ''
+
+        for l in line_buffered(ssh_stdout):
+            print(l)
+            out += l
+
+        return out
 
     @staticmethod
     def deploy(username="trevor", host="trevor1.local"):
